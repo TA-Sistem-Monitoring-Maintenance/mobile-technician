2a1888358c491c2c26095ea1b383e944285cbdf4
naja-kalachakra <naja@kalachakra.io>
2025-04-29 21:24:43 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <div class="bg-gray/100 sm:px-4 md:px-6 h-screen">
+    <div class="mx-auto max-w-[425px] bg-white p-1 h-screen">
+      <div class="flex flex-rows justify-between">
+        <p>LALALAL</p>
+        <p>Jalalal</p>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/main.js
@@ -0,0 +1,32 @@
+// import { createApp } from "vue";
+// import App from "./App.vue";
+// import "./main.css";
+// createApp(App).mount("#app");
+
+import { createApp } from "vue";
+import App from "./App.vue";
+// import router from "./router/index";
+import "./main.css"; // Tailwind's global styles
+import "./style.css";
+// import "../node_modules/simplebar/dist/simplebar.min.css";
+
+// import vuetify from "../plugins/vuetify"; // Vuetify plugin
+// import "vuetify/styles"; // Import Vuetify styles globally
+
+// import PrimeVue from "primevue/config";
+// import AutoComplete from "primevue/autocomplete";
+
+// import VueCookies from "vue-cookies";
+
+const app = createApp(App);
+
+// Use the necessary plugins
+// app.use(VueCookies);
+// app.use(router);
+// app.use(PrimeVue);
+// app.use(vuetify);
+
+// Register components globally if needed
+// app.component("AutoComplete", AutoComplete);
+
+app.mount("#app");
