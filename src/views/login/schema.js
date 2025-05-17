fa393a66e848f195baee95cc5ca605be1ac4de12
aryafridayana <aryafridayana20@gmail.com>
2025-05-18 01:05:23 +0700

    signup2

--- src/views/login/schema.js
+++ src/views/login/schema.js
@@ -11,3 +11,35 @@ export const loginSchema = yup.object({
   username: yup.string().required("Username wajib diisi"),
   password: yup.string().required("Password wajib diisi"),
 });
+
+// Define signup schema
+export const signupSchema = yup.object({
+  name: yup.string().required("Name is required"),
+  
+  username: yup
+    .string()
+    .email("Please enter a valid email address")
+    .required("Email is required"),
+  
+  password: yup
+    .string()
+    .min(8, "Password must be at least 8 characters")
+    .matches(/[A-Z]/, "Password must contain at least one uppercase letter")
+    .matches(/[a-z]/, "Password must contain at least one lowercase letter")
+    .matches(/[0-9]/, "Password must contain at least one number")
+    .matches(
+      /[!@#$%^&*(),.?":{}|<>]/,
+      "Password must contain at least one special character"
+    )
+    .required("Password is required"),
+
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref("password"), null], "Passwords must match")
+    // .required("Confirm Password confirmation is required"),
+});
+
+export const validationSchema = (isSignup) => {
+  // Return the appropriate schema based on whether it's login or signup
+  return isSignup ? signupSchema : loginSchema;
+};
--- src/views/login/service.js
+++ src/views/login/service.js
@@ -1,5 +1,7 @@
 import { post } from "../../utils/networkUtils";
+import { signupSchema } from "./schema";
 
 export const Service = {
   login: async (data) => await post("/v1/auth/", data),
+  signup: async (data) => await post("/v1/auth/", data),
 };
