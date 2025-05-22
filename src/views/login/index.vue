348188eb5de5a36c734d31b0fe7b4a97c2c2ae4f
naja-kalachakra <naja@kalachakra.io>
2025-05-22 23:52:47 +0700

    dfgioijafdioakfoidsjafd

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,248 +1,101 @@
 <script setup>
-import { ref, computed, watch, onMounted, provide } from "vue";
+import { ref } from "vue";
 import { useRouter } from "vue-router";
-import axios from "axios";
-import CryptoJS from "crypto-js";
-import VueCookies from "vue-cookies";
-import MyToaster from "@components/Toaster/MyToaster"; // Assuming you have a toaster component for messages
+import { handleLogin, handleSignUp } from "./store.js";
 import MyButton from "@components/Button/MyButton.vue";
 import MyTextField from "@components/TextField/MyTextField.vue";
+import { loginSchema, signupSchema } from "./schema.js"; // Import the schema
 import MyButtonGroupV2 from "@components/Button/MyButtonGroupV2.vue";
-import { Check } from "untitledui-js/vue"; // Assuming you're using an icon component for checkmarks
 
-// Define form fields and errors
-const email = ref("");
+// State untuk username, password, error message, dan loading
+const username = ref("");
 const password = ref("");
-const confirmPassword = ref("");
-const name = ref(""); // for signup name field
-const usernameError = ref("");
-const passwordError = ref("");
-const nameError = ref(""); // for signup name error
-const confirmPasswordError = ref(""); // for signup confirm password error
+const name = ref("");
+const confirm_password = ref("");
+const usernameError = ref(""); // Error message for username
+const passwordError = ref(""); // Error message for password
 const isLoading = ref(false);
-const isSignup = ref(false); // toggle between login and signup
-
-// Password validation checks
-const isLongEnough = computed(
-  () => password.value && password.value.length >= 8
-);
-const hasSpecialChar = computed(
-  () => password.value && /[!@#$%^&*(),.?":{}|<>]/.test(password.value)
-);
-const passwordsMatch = computed(
-  () =>
-    password.value &&
-    confirmPassword.value &&
-    password.value === confirmPassword.value
-);
+const selectedTab = ref("login");
+const confirm_passwordError = ref("");
+const nameError = ref("");
 
 const router = useRouter();
 
-// Handle login
-async function handleLogin() {
-  try {
-    const encryptedPassword = CryptoJS.AES.encrypt(
-      password.value,
-      import.meta.env.VITE_APP_SECRET_KEY
-    ).toString();
-
-    // Prepare form data
-    const formData = new FormData();
-    formData.append("email", email.value);
-    formData.append("password", encryptedPassword);
-
-    // Get the timezone
-    const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-
-    const response = await axios.post(
-      "http://localhost:3000/monitoring/v1/auth/",
-      formData,
-      {
-        headers: {
-          "Content-Type": "multipart/form-data",
-          "Time-Zone": timeZone,
-        },
-      }
-    );
-
-    VueCookies.set("tokenMonitoringMobile", response.data.token, "1d");
-    MyToaster({
-      message: "Login successful!",
-      type: "success",
-    });
-    router.push("/task");
-  } catch (error) {
-    MyToaster({
-      message: error.response?.data?.message || error.message || "Login failed",
-      type: "error",
-    });
-    console.error("Login failed:", error.response?.data || error.message);
-  }
-}
-
-// Handle sign-up
-async function handleSignUp() {
-  try {
-    const encryptedPassword = CryptoJS.AES.encrypt(
-      password.value,
-      import.meta.env.VITE_APP_SECRET_KEY
-    ).toString();
-
-    // Prepare form data
-    const formData = new FormData();
-    formData.append("email", email.value);
-    formData.append("password", encryptedPassword);
-    formData.append("name", name.value); // Send user's name
-
-    const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-
-    const response = await axios.post(
-      "http://localhost:3000/monitoring/v1/mobile/auth/register", // Adjust your URL for sign-up
-      formData,
-      {
-        headers: {
-          "Content-Type": "multipart/form-data",
-          "Time-Zone": timeZone,
-        },
-      }
-    );
-
-    MyToaster({
-      message: "Registration successful! You can now log in.",
-      type: "success",
-    });
-
-    VueCookies.set("tokenMonitoringMobile", response.data.token, "1d");
-    router.push("/login"); // Redirect to login after successful registration
-  } catch (error) {
-    MyToaster({
-      message:
-        error.response?.data?.message || error.message || "Sign-Up failed",
-      type: "error",
-    });
-    console.error("Sign-Up failed:", error.response?.data || error.message);
-  }
-}
-
-// Handle submit
 const onSubmit = async () => {
-  usernameError.value = "";
-  passwordError.value = "";
-  nameError.value = "";
-  confirmPasswordError.value = "";
-  isLoading.value = true;
+  usernameError.value = ""; // Reset username error
+  passwordError.value = ""; // Reset password error
+  isLoading.value = true; // Tampilkan spinner loading
 
-  try {
-    if (isSignup.value) {
-      // Handle signup
-      await signupSchema.validate(
+  if (selectedTab.value === "login") {
+    try {
+      await loginSchema.validate(
         {
-          name: name.value,
-          username: email.value, // Using email as username
+          username: username.value,
           password: password.value,
         },
-        { abortEarly: false }
+        { abortEarly: false } // Collect all errors
       );
-
-      // Call handleSignUp to trigger the sign-up action
+      await handleLogin({
+        username: username.value,
+        password: password.value,
+        router,
+      });
+    } catch (error) {
+      if (error.inner) {
+        // Map validation errors to specific fields
+        error.inner.forEach((err) => {
+          if (err.path === "username") {
+            usernameError.value = err.message;
+          } else if (err.path === "password") {
+            passwordError.value = err.message;
+          }
+        });
+      }
+    } finally {
+      isLoading.value = false; // Hentikan spinner loading
+    }
+  } else {
+    try {
+      // await signupSchema.validate(
+      //   {
+      //     username: username.value,
+      //     password: password.value,
+      //     name: name.value,
+      //     confirm_password: confirm_password?.value,
+      //   },
+      //   { abortEarly: false } // Collect all errors
+      // );
       await handleSignUp({
-        username: email.value,
+        username: username.value,
         password: password.value,
         name: name.value,
         router,
       });
-
-      // Reset fields after successful signup
-      name.value = "";
-      email.value = "";
-      password.value = "";
-      confirmPassword.value = "";
-
-      // Switch to login form after signup
-      isSignup.value = false;
-    } else {
-      // Handle login
-      await loginSchema.validate(
-        { username: email.value, password: password.value },
-        { abortEarly: false }
-      );
-      await handleLogin({
-        username: email.value,
-        password: password.value,
-        router,
-      });
+    } catch (error) {
+      if (error.inner) {
+        // Map validation errors to specific fields
+        error.inner.forEach((err) => {
+          if (err.path === "username") {
+            usernameError.value = err.message;
+          } else if (err.path === "password") {
+            passwordError.value = err.message;
+          } else if (err.path === "name") {
+            nameError.value = err.message;
+          } else if (err.path === "confirm_password") {
+            confirm_passwordError.value = err.message;
+          }
+        });
+      }
+    } finally {
+      isLoading.value = false; // Hentikan spinner loading
     }
-  } catch (error) {
-    if (error.inner) {
-      error.inner.forEach((err) => {
-        if (err.path === "username") {
-          usernameError.value = err.message;
-        } else if (err.path === "password") {
-          passwordError.value = err.message;
-        } else if (err.path === "name") {
-          nameError.value = err.message;
-        } else if (err.path === "confirmPassword") {
-          confirmPasswordError.value = err.message;
-        }
-      });
-    }
-
-    // Use toaster for error message
-    MyToaster({
-      message: "An error occurred. Please try again.",
-      type: "error",
-    });
-  } finally {
-    isLoading.value = false;
   }
 };
-
-// Function to toggle between Login and Sign Up forms
-const toggleForm = (formType) => {
-  isSignup.value = formType === "signup";
-};
-
-// Watch for changes to isSignup and clear the form fields
-watch(isSignup, (newValue) => {
-  if (!newValue) {
-    // If switching to login, reset the fields
-    email.value = "";
-    password.value = "";
-    confirmPassword.value = "";
-    name.value = "";
-    usernameError.value = "";
-    passwordError.value = "";
-    nameError.value = "";
-    confirmPasswordError.value = "";
-  }
-});
-
-// Reset email and password to null when the component is mounted
-onMounted(() => {
-  email.value = ""; // Set email to null on mount
-  password.value = ""; // Set password to null on mount
-  confirmPassword.value = ""; // Set confirm password to null on mount
-});
-
-// Provide login and signup functionality and data to child components
-provide("auth", {
-  email,
-  password,
-  confirmPassword,
-  name,
-  isSignup,
-  isLongEnough,
-  hasSpecialChar,
-  passwordsMatch,
-  handleLogin,
-  handleSignUp,
-  onSubmit,
-  toggleForm,
-});
 </script>
 
 <template>
   <main class="flex h-screen">
+    <!-- Bagian Kiri -->
     <section
       class="w-full bg-white flex flex-col justify-center items-center p-8 md:p-12"
     >
@@ -253,143 +106,148 @@ provide("auth", {
           class="w-209 h-32 mx-auto mb-4"
         />
         <h1 class="text-3xl font-bold text-gray-900 mb-2">
-          {{ isSignup ? "Create an account" : "Log in to your account" }}
+          Log in to your account
         </h1>
-        <p class="text-gray-600">
-          {{
-            isSignup
-              ? "Create an account below"
-              : "Welcome back! Please enter your details."
-          }}
-        </p>
+        <p class="text-gray-600">Welcome back! Please enter your details.</p>
       </div>
-
-      <!-- Login / Signup Tabs -->
-      <div class="flex justify-center p-4">
+      <div>
         <MyButtonGroupV2
           :buttons="[
-            { label: 'Sign in', value: 'login' },
-            { label: 'Sign up', value: 'signup' },
+            { label: 'Login', value: 'login' },
+            { label: 'Register', value: 'register' },
           ]"
-          :value="isSignup ? 'signup' : 'login'"
-          :onChange="(val) => toggleForm(val)"
+          :value="selectedTab"
+          :onChange="
+            (val) => {
+              selectedTab = val;
+            }
+          "
         />
       </div>
-
-      <!-- Login or Signup Form -->
-      <form @submit.prevent="onSubmit" class="w-full max-w-sm">
-        <div v-if="isSignup" class="mb-6">
-          <label for="name" class="block text-sm font-medium text-gray-700"
-            >Name</label
-          >
-          <MyTextField
-            type="text"
-            id="name"
-            v-model="name"
-            placeholder="Enter your name"
-            :error-message="nameError"
-          />
-        </div>
-
-        <div class="mb-6">
-          <label for="username" class="block text-sm font-medium text-gray-700"
-            >Email</label
-          >
-          <MyTextField
-            type="email"
-            id="username"
-            v-model="email"
-            placeholder="Enter your email"
-            :error-message="usernameError"
-          />
-        </div>
-
-        <div class="mb-6">
-          <label for="password" class="block text-sm font-medium text-gray-700"
-            >Password</label
-          >
-          <MyTextField
-            type="password"
-            id="password"
-            v-model="password"
-            placeholder="Enter your password"
-            :error-message="passwordError"
-          />
-        </div>
-
-        <!-- Show confirm password only for sign-up form -->
-        <div v-if="isSignup" class="mb-6">
-          <label
-            for="confirmPassword"
-            class="block text-sm font-medium text-gray-700"
-            >Confirm Password</label
-          >
-          <MyTextField
-            type="password"
-            id="confirmPassword"
-            v-model="confirmPassword"
-            placeholder="Confirm your password"
-            :error-message="confirmPasswordError"
-          />
-        </div>
-
-        <!-- Password validation requirements -->
-        <div v-if="isSignup" class="flex flex-col gap-y-3">
-          <div class="flex flex-row gap-2 items-center">
-            <Check
-              :size="20"
-              :color="isLongEnough ? 'green' : 'currentColor'"
+      <form @submit.prevent="onSubmit" class="mt-10 w-full max-w-sm">
+        <div v-if="selectedTab === 'register'">
+          <div class="mb-6">
+            <label for="name" class="block text-sm font-medium text-gray-700">
+              Name
+            </label>
+            <MyTextField
+              type="text"
+              id="name"
+              v-model="name"
+              placeholder="Enter your email"
+              :error-message="nameError"
             />
-            <p :class="[isLongEnough ? 'text-green-600' : 'text-gray-500']">
-              Must be at least 8 characters
-            </p>
           </div>
-          <div class="flex flex-row gap-2 items-center">
-            <Check
-              :size="20"
-              :color="hasSpecialChar ? 'green' : 'currentColor'"
+          <div class="mb-6">
+            <label
+              for="username"
+              class="block text-sm font-medium text-gray-700"
+            >
+              Email
+            </label>
+            <MyTextField
+              type="text"
+              id="username"
+              v-model="username"
+              placeholder="Enter your email"
+              :error-message="usernameError"
             />
-            <p :class="[hasSpecialChar ? 'text-green-600' : 'text-gray-500']">
-              Must contain one special character
-            </p>
           </div>
-          <div class="flex flex-row gap-2 items-center">
-            <Check
-              :size="20"
-              :color="passwordsMatch ? 'green' : 'currentColor'"
+          <div class="mb-6">
+            <label
+              for="password"
+              class="block text-sm font-medium text-gray-700"
+            >
+              Password
+            </label>
+            <MyTextField
+              type="password"
+              id="password"
+              v-model="password"
+              placeholder="Enter your password"
+              :error-message="passwordError"
             />
-            <p :class="[passwordsMatch ? 'text-green-600' : 'text-gray-500']">
-              Passwords match
-            </p>
+          </div>
+          <div class="mb-6">
+            <label
+              for="email"
+              class="block text-sm font-medium text-gray/700 pb-2"
+            >
+              Confirm password
+            </label>
+            <MyTextField
+              type="password"
+              id="confirm_password"
+              v-model="confirm_password"
+              :error-message="confirm_passwordError"
+              placeholder="Confirm new password"
+            />
+          </div>
+          <div class="flex justify-center">
+            <MyButton
+              expanded=""
+              class="w-full py-3 rounded-lg"
+              color="primary"
+              variant="filled"
+              :disabled="isLoading"
+              @click="onSubmit"
+            >
+              <p class="text-md-semibold">Register</p>
+            </MyButton>
           </div>
         </div>
 
-        <div class="flex justify-end items-center mb-6" v-if="!isSignup">
-          <router-link
-            to="/forgot-password"
-            class="text-sm-semibold text-purple-600 hover:underline"
-            >Forgot password</router-link
-          >
-        </div>
-
-        <div class="flex justify-center mt-4">
-          <MyButton
-            expanded=""
-            class="w-full py-3 rounded-lg"
-            color="primary"
-            variant="filled"
-            :disabled="
-              isLoading ||
-              (isSignup
-                ? !isLongEnough || !hasSpecialChar || !passwordsMatch
-                : !email || !password)
-            "
-            type="submit"
-          >
-            <p class="text-md-semibold">
-              {{ isSignup ? "Get started" : "Sign in" }}
-            </p>
-          </MyButton>
+        <div v-else>
+          <div class="mb-6">
+            <label
+              for="username"
+              class="block text-sm font-medium text-gray-700"
+            >
+              Email
+            </label>
+            <MyTextField
+              type="text"
+              id="username"
+              v-model="username"
+              placeholder="Enter your email"
+              :error-message="usernameError"
+            />
+          </div>
+          <div class="mb-6">
+            <label
+              for="password"
+              class="block text-sm font-medium text-gray-700"
+            >
+              Password
+            </label>
+            <MyTextField
+              type="password"
+              id="password"
+              v-model="password"
+              placeholder="Enter your password"
+              :error-message="passwordError"
+            />
+          </div>
+          <div class="flex justify-end items-center mb-6">
+            <router-link
+              to="/forgot-password"
+              class="text-sm-semibold text-purple-600 hover:underline"
+            >
+              Forgot password
+            </router-link>
+          </div>
+          <div class="flex justify-center">
+            <MyButton
+              expanded=""
+              class="w-full py-3 rounded-lg"
+              color="primary"
+              variant="filled"
+              :disabled="isLoading"
+              @click="onSubmit"
+            >
+              <p class="text-md-semibold">Sign in</p>
+            </MyButton>
+          </div>
         </div>
       </form>
     </section>
--- src/views/login/store.js
+++ src/views/login/store.js
@@ -1,95 +1,96 @@
-// // store.js
-// import { ref } from "vue";
-// import axios from "axios";
-// import CryptoJS from "crypto-js";
-// import VueCookies from "vue-cookies";
-// import MyToaster from "@components/Toaster/MyToaster"; // Pastikan Anda sudah punya komponen MyToaster
+// store.js
+import { ref } from "vue";
+import axios from "axios";
+import CryptoJS from "crypto-js";
+import VueCookies from "vue-cookies";
+import MyToaster from "@components/Toaster/MyToaster"; // Pastikan Anda sudah punya komponen MyToaster
 
-// // Login and SignUp Logic
-// export const email = ref("");
-// export const password = ref("");
+// Login and SignUp Logic
+export const email = ref("");
+export const password = ref("");
 
-// // Handle Login Function
-// export async function handleLogin(body) {
-//   try {
-//     const encryptedPassword = CryptoJS.AES.encrypt(
-//       body.password,
-//       import.meta.env.VITE_APP_SECRET_KEY
-//     ).toString();
+// Handle Login Function
+export async function handleLogin(body) {
+  try {
+    const encryptedPassword = CryptoJS.AES.encrypt(
+      body.password,
+      import.meta.env.VITE_APP_SECRET_KEY
+    ).toString();
 
-//     // Prepare form data
-//     const formData = new FormData();
-//     formData.append("email", body.username);
-//     formData.append("password", encryptedPassword);
+    // Prepare form data
+    const formData = new FormData();
+    formData.append("email", body.username);
+    formData.append("password", encryptedPassword);
 
-//     // Get timezone
-//     const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    // Get timezone
+    const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
-//     const response = await axios.post(
-//       "http://localhost:3000/monitoring/v1/auth/",
-//       formData,
-//       {
-//         headers: {
-//           "Content-Type": "multipart/form-data",
-//           "Time-Zone": timeZone,
-//         },
-//       }
-//     );
+    const response = await axios.post(
+      "http://localhost:3000/monitoring/v1/auth/",
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          "Time-Zone": timeZone,
+        },
+      }
+    );
 
-//     // Show success message using toaster
-//     MyToaster({
-//       message: "Login successful!",
-//       type: "success",
-//     });
+    // Show success message using toaster
+    MyToaster({
+      message: "Login successful!",
+      type: "success",
+    });
 
-//     VueCookies.set("tokenMonitoringMobile", response.data.token, "1d");
-//     body.router.push("/task");
-//   } catch (error) {
-//     MyToaster({
-//       message: error.response?.data?.message || error.message || "Login failed",
-//       type: "error",
-//     });
-//   }
-// }
+    VueCookies.set("tokenMonitoringMobile", response.data.token, "1d");
+    body.router.push("/task");
+  } catch (error) {
+    MyToaster({
+      message: error.response?.data?.message || error.message || "Login failed",
+      type: "error",
+    });
+  }
+}
 
-// // Handle SignUp Function
-// export async function handleSignUp(body) {
-//   try {
-//     const encryptedPassword = CryptoJS.AES.encrypt(
-//       body.password,
-//       import.meta.env.VITE_APP_SECRET_KEY
-//     ).toString();
+// Handle SignUp Function
+export async function handleSignUp(body) {
+  console.log(body);
+  try {
+    const encryptedPassword = CryptoJS.AES.encrypt(
+      body.password,
+      import.meta.env.VITE_APP_SECRET_KEY
+    ).toString();
 
-//     // Prepare form data
-//     const formData = new FormData();
-//     formData.append("email", body.username);
-//     formData.append("password", encryptedPassword);
-//     formData.append("name", body.name);
+    // Prepare form data
+    const formData = new FormData();
+    formData.append("email", body.username);
+    formData.append("password", body?.password);
+    formData.append("name", body.name);
 
-//     const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
-//     const response = await axios.post(
-//       "http://localhost:3000/monitoring/v1/mobile/auth/register", // Adjust URL for registration
-//       formData,
-//       {
-//         headers: {
-//           "Content-Type": "multipart/form-data",
-//           "Time-Zone": timeZone,
-//         },
-//       }
-//     );
+    const response = await axios.post(
+      "http://localhost:3000/monitoring/v1/mobile/auth/register", // Adjust URL for registration
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          "Time-Zone": timeZone,
+        },
+      }
+    );
 
-//     MyToaster({
-//       message: "Registration successful! You can now log in.",
-//       type: "success",
-//     });
+    MyToaster({
+      message: "Registration successful! You can now log in.",
+      type: "success",
+    });
 
-//     VueCookies.set("tokenMonitoringMobile", response.data.token, "1d");
-//     body.router.push("/welcome"); // Redirect on success
-//   } catch (error) {
-//     MyToaster({
-//       message: error.response?.data?.message || error.message || "Sign-Up failed",
-//       type: "error",
-//     });
-//   }
-// }
+    body.router.push("/login"); // Redirect on success
+  } catch (error) {
+    MyToaster({
+      message:
+        error.response?.data?.message || error.message || "Sign-Up failed",
+      type: "error",
+    });
+  }
+}
