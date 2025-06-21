a518dabeef39436ec0ed9b1f272ed217ffa30ad0
naja-kalachakra <naja@kalachakra.io>
2025-06-21 17:01:02 +0700

    mobile

--- src/views/User/scan/complaint/index.vue
+++ src/views/User/scan/complaint/index.vue
@@ -100,106 +100,104 @@ watchEffect(async () => {
   >
     <template #element><importSlider /> </template>
   </MyModalSlider>
-  <simplebar class="h-full" forceVisible="y" autoHide="{false}">
-    <div class="bg-white">
-      <div class="pb-3 gap-3">
-        <p class="text-lg-semibold text-gray/900">
-          Maintenance Submission Form
-        </p>
-        <p class="text-sm-regular text-gray/600 pb-2">maintenance requests</p>
-        <hr class="py-1" />
-      </div>
-      <div class="flex flex-col gap-4">
-        <div class="flex flex-col gap-2">
-          <label
-            class="text-sm-medium text-gray/700 after:text-red/600 after:content-['*']"
-          >
-            Room
-          </label>
-          <MyAsyncDropdown
-            class="w-full"
-            name="Room"
-            :placeholder="'Select Room'"
-            v-model="selectedRoom"
-            :disabled="true"
-          />
-        </div>
-        <div class="flex flex-col gap-2">
-          <label
-            class="text-sm-medium text-gray/700 after:text-red/600 after:content-['*']"
-          >
-            Location
-          </label>
-          <MyAsyncDropdown
-            class="w-full"
-            name="Location"
-            :placeholder="'Select Location'"
-            v-model="selectedLocation"
-            :disabled="true"
-          />
-        </div>
-        <div class="flex flex-col gap-2">
-          <label
-            class="text-sm-medium text-gray/700 after:text-red/600 after:content-['*']"
-          >
-            Equipment
-          </label>
-          <MyAsyncDropdown
-            class="w-full"
-            name="asset"
-            :placeholder="'Select Equipment'"
-            v-model="asset"
-            :extraData="selectedRoom?.id"
-            :asyncFunction="searchAsset"
-            :getOptionLabel="(tag) => tag?.name"
-          />
-        </div>
-        <div class="flex flex-col gap-2">
-          <label
-            class="text-sm-medium text-gray/700 after:text-red/600 after:content-['*']"
-          >
-            Reason
-          </label>
-          <MyTextArea
-            placeholder="Enter a reason..."
-            v-model="reason"
-            :errorMessage="error"
-            :helperText="error"
-            :maxLength="500"
-            onDropAccepted=""
-          />
-        </div>
-        <div class="flex flex-col gap-2">
-          <MyDropzone
-            ref="dropzoneRef"
-            :multiple="true"
-            :maxSize="2_500_000"
-            :showImage="true"
-            :onChange="handleFilesChange"
-          />
-        </div>
-        <div
-          class="sticky bottom-0 left-0 right-0 bg-white z-10 shadow-t px-4 py-3 flex justify-end gap-2 border-t"
+  <!-- <simplebar class="h-full" forceVisible="y" autoHide="{false}"> -->
+  <div class="bg-white">
+    <div class="pb-3 gap-3">
+      <p class="text-lg-semibold text-gray/900">Maintenance Submission Form</p>
+      <p class="text-sm-regular text-gray/600 pb-2">maintenance requests</p>
+      <hr class="py-1" />
+    </div>
+    <div class="flex flex-col gap-4">
+      <div class="flex flex-col gap-2">
+        <label
+          class="text-sm-medium text-gray/700 after:text-red/600 after:content-['*']"
         >
-          <MyButton
-            color="secondary"
-            variant="outlined"
-            size="md"
-            @click="goToHistory"
-          >
-            <p class="text-sm-semibold">History</p>
-          </MyButton>
-          <MyButton
-            type="submit"
-            color="primary"
-            variant="filled"
-            size="md"
-            @click="handleSubmit"
-          >
-            <p class="text-sm-semibold">Send</p>
-          </MyButton>
-        </div>
+          Room
+        </label>
+        <MyAsyncDropdown
+          class="w-full"
+          name="Room"
+          :placeholder="'Select Room'"
+          v-model="selectedRoom"
+          :disabled="true"
+        />
+      </div>
+      <div class="flex flex-col gap-2">
+        <label
+          class="text-sm-medium text-gray/700 after:text-red/600 after:content-['*']"
+        >
+          Location
+        </label>
+        <MyAsyncDropdown
+          class="w-full"
+          name="Location"
+          :placeholder="'Select Location'"
+          v-model="selectedLocation"
+          :disabled="true"
+        />
+      </div>
+      <div class="flex flex-col gap-2">
+        <label
+          class="text-sm-medium text-gray/700 after:text-red/600 after:content-['*']"
+        >
+          Equipment
+        </label>
+        <MyAsyncDropdown
+          class="w-full"
+          name="asset"
+          :placeholder="'Select Equipment'"
+          v-model="asset"
+          :extraData="selectedRoom?.id"
+          :asyncFunction="searchAsset"
+          :getOptionLabel="(tag) => tag?.name"
+        />
+      </div>
+      <div class="flex flex-col gap-2">
+        <label
+          class="text-sm-medium text-gray/700 after:text-red/600 after:content-['*']"
+        >
+          Reason
+        </label>
+        <MyTextArea
+          placeholder="Enter a reason..."
+          v-model="reason"
+          :errorMessage="error"
+          :helperText="error"
+          :maxLength="500"
+          onDropAccepted=""
+        />
+      </div>
+      <div class="flex flex-col gap-2">
+        <MyDropzone
+          ref="dropzoneRef"
+          :multiple="true"
+          :maxSize="2_500_000"
+          :showImage="true"
+          :onChange="handleFilesChange"
+        />
+      </div>
+      <div
+        class="sticky bottom-0 left-0 right-0 bg-white z-10 shadow-t px-4 py-3 flex justify-end gap-2 border-t"
+      >
+        <MyButton
+          color="secondary"
+          variant="outlined"
+          size="md"
+          @click="goToHistory"
+        >
+          <p class="text-sm-semibold">History</p>
+        </MyButton>
+        <MyButton
+          type="submit"
+          color="primary"
+          variant="filled"
+          size="md"
+          @click="handleSubmit"
+        >
+          <p class="text-sm-semibold">Send</p>
+        </MyButton>
       </div>
     </div>
-  </simplebar>
+  </div>
+  <!-- </simplebar> -->
 </template>
